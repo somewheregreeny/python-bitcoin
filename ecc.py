9929247a535340b20aed19b1b8145e7b0beb0096
이서윤 <idfn3535@gmail.com>
2022-06-05 01:52:01 +0900

    Feat : 서명, 비밀키 추가

--- ecc.py
+++ ecc.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # 유한체의 원소 하나
 class FieldElement:
 
@@ -138,6 +140,7 @@ class S256Field(FieldElement):
         return '{:x}'.format(self.num).zfill(64)
 
 
+# secp256k1의 함수
 A = 0
 B = 7
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
@@ -156,7 +159,44 @@ class S256Point(Point):
         coef = coefficient % N
         return super().__rmul__(coef)
 
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
+
 
 G = S256Point(
     0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
     0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature({:x}, {:x})'.format(self.r, self.s)
+
+
+class PrivateKey:
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return '{:x}'.format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = randint(0, N)
+        r = (k * G).x.num
+        k_inv = pow(k, N-2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N/2:
+            s = N - s
+        return Signature(r, s)
