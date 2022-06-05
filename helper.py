e439d2f864e6e21431f69ac64fadb41babd22364
이서윤 <idfn3535@gmail.com>
2022-06-05 14:04:32 +0900

    Feat : hash, base encode

---
+++ helper.py
@@ -0,0 +1,68 @@
+import hashlib
+
+
+# tag::source1[]
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+# end::source1[]
+
+
+# tag::source4[]
+def hash160(s):
+    '''sha256 followed by ripemd160'''
+    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()  # <1>
+# end::source4[]
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+# tag::source2[]
+def encode_base58(s):
+    count = 0
+    for c in s:  # <1>
+        if c == 0:
+            count += 1
+        else:
+            break
+    num = int.from_bytes(s, 'big')
+    prefix = '1' * count
+    result = ''
+    while num > 0:  # <2>
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+    return prefix + result  # <3>
+# end::source2[]
+
+
+# tag::source3[]
+def encode_base58_checksum(b):
+    return encode_base58(b + hash256(b)[:4])
+# end::source3[]
+
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
+def little_endian_to_int(b):
+    '''little_endian_to_int takes byte sequence as a little-endian number.
+    Returns an integer'''
+    # use int.from_bytes()
+    raise NotImplementedError
+
+
+def int_to_little_endian(n, length):
+    '''endian_to_little_endian takes an integer and returns the little-endian
+    byte sequence of length'''
+    # use n.to_bytes()
+    raise NotImplementedError
