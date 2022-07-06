185caddbb912e307cc5ccfac6104afcb0cde92d5
이서윤 <idfn3535@gmail.com>
2022-07-06 23:29:07 +0900

    10

---
+++ network.py
@@ -0,0 +1,145 @@
+import socket
+import time
+
+from io import BytesIO
+from random import randint
+
+from block import Block
+from helper import (
+    hash256,
+    encode_varint,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint,
+)
+
+NETWORK_MAGIC = b'\xf9\xbe\xb4\xd9'
+TESTNET_NETWORK_MAGIC = b'\x0b\x11\x09\x07'
+
+class NetWorkEnvelope:
+
+    def __init__(self, command, payload, testnet=False):
+        self.command = command
+        self.payload = payload
+        if testnet:
+            self.magic = TESTNET_NETWORK_MAGIC
+        else:
+            self.magic = NETWORK_MAGIC
+
+    def __repr__(self):
+        return '{} : {}'.format(
+            self.command.decode('ascii'),
+            self.payload.hex(),
+        )
+
+    @classmethod
+    def parse(cls, s, testnet=False):
+        magic = s.read(4)
+        if magic == b'':
+            raise IOError('Connection reset!')
+
+        if testnet:
+            expected_magic = TESTNET_NETWORK_MAGIC
+        else:
+            expected_magic = NETWORK_MAGIC
+
+        if magic != expected_magic:
+            raise SyntaxError('magic is not right {} vs {}'.format(magic.hex(),
+                                                                   expected_magic.hex()))
+        command = s.read(12)
+        command = command.strip(b'\x00')
+        payload_length = little_endian_to_int(s.read(4))
+        checksum = s.read(4)
+        payload = s.read(payload_length)
+
+        calculated_checksum = hash256(payload)[:4]
+        if calculated_checksum != checksum:
+            raise IOError('checksum does not match')
+        return cls(command, payload, testnet=testnet)
+
+    def serialize(self):
+        result = self.magic
+        result += self.command + b'\x00' * (12 - len(self.command))
+        result += int_to_little_endian(len(self.payload), 4)
+        result += hash256(self.payload)[:4]
+        result += self.payload
+        return result
+
+
+class VersionMessage:
+    command = b'version'
+
+    def __init__(self, version=70015, services=0, timestamp=None,
+                 receiver_services=0,
+                 receiver_ip=b'\x00\x00\x00\x00', receiver_port=8333,
+                 sender_services=0,
+                 sender_ip=b'\x00\x00\x00\x00', sender_port=8333,
+                 nonce=None, user_agent=b'/programmingbitcoin:0.1/',
+                 latest_block=0, relay=False):
+        self.version = version
+        self.services = services
+        if timestamp is None:
+            self.timestamp = int(time.time())
+        else:
+            self.timestamp = timestamp
+        self.receiver_services = receiver_services
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port
+        self.sender_services = sender_services
+        self.sender_ip = sender_ip
+        self.sender_port = sender_port
+        if nonce is None:
+            self.nonce = int_to_little_endian(randint(0, 2**64), 8)
+        else:
+            self.nonce = nonce
+        self.user_agent = user_agent
+        self.latest_block = latest_block
+        self.relay = relay
+
+    def serialize(self):
+        result = int_to_little_endian(self.version, 4)
+        result += int_to_little_endian(self.services, 8)
+        result += int_to_little_endian(self.timestamp, 8)
+        result += int_to_little_endian(self.receiver_services, 8)
+        result += b'\x00' * 10 + b'\xff\xff' + self.receiver_ip
+        result += int_to_little_endian(self.receiver_port, 2)
+        result += int_to_little_endian(self.sender_services, 8)
+        result += b'\x00' * 10 + b'\xff\xff' + self.sender_ip
+        result += int_to_little_endian(self.sender_port, 2)
+        result += self.nonce
+        result += encode_varint(len(self.user_agent))
+        result += self.user_agent
+        result += int_to_little_endian(self.latest_block, 4)
+        if self.relay:
+            result += b'\x01'
+        else:
+            result += b'\x00'
+        return result
+
+
+    class VerAckMessage:
+        command = b'verack'
+
+        def __init__(self):
+            pass
+
+        @classmethod
+        def parse(cls, s):
+            return cls()
+
+        def serialize(self):
+            return b''
+
+
+    class SimpleNode:
+
+        def __init__(self, host, port=None, testnet=False, logging=False):
+            if port is None:
+                if testnet:
+                    port = 18333
+                else:
+                    port = 8333
+            self.testnet = testnet
+            self.logging = logging
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            
